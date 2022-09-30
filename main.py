33454395f320facd0b287557dc58255c10c33ada
filliona <filliona@educ.college-montreal.qc.ca>
2022-09-30 09:13:27 -0400

    commit initial

--- main.py
+++ main.py
@@ -4,30 +4,45 @@ Groupe 023
 Description: TP2 - Jeu de devinettes
 """
 
-boucle_jeu = True
 import random
 
-nombre_aleatoire = random.randint(0, 100)
-print("J'ai choisi un nombre au hasard entre 0 et 100")
-print("À vous de le deviner !")
+start_jeu = True
+nb_minimum = 0
+nb_maximum = 100
 
+def bornes():
+    global nb_minimum, nb_maximum
+    nb_minimum = int(input("Décidez le nombre minimum que je peux choisir:"))
+    nb_maximum = int(input("Décider le nombre maximum que je peux choisir:"))
 
-def jeu():
+while start_jeu:
+    print("Mon but est de vous faire deviner un nombre que j'ai choisi aléatoirement.")
+    bornes()
+    nombre_aleatoire = random.randint(nb_minimum, nb_maximum)
+    print(f"Parfait, j'ai choisi un nombre entre {nb_minimum} et {nb_maximum}")
+    print("Maintenant bonne chance pour le deviner !")
     nombre_essai = 1
-    essai = input(int(f"Essai numéro {nombre_essai}, Entrez un nombre:"))
-    if essai == nombre_aleatoire:
-        print("Bravo! Vous avez trouvé.")
-        rejouer = input(str("Voulez-vous rejouez ?")
-        if rejouer == "oui":
-            jeu()
-        else:
-            boucle_jeu = False
-    elif essai < nombre_aleatoire:
-        print("Raté, pensez plus grand")
-        nombre_essai = nombre_essai+1
-        jeu()
-    elif essai > nombre_aleatoire:
-        print("Raté, pensez plus petit")
-        nombre_essai = nombre_essai + 1
-        jeu()
-while boucle_jeu:
+    boucle_jeu = True
+    while boucle_jeu == True:
+        essai = int(input(f"Essai numéro {nombre_essai}, Entrez un nombre:"))
+        nombre_essai += 1
+        if essai < nombre_aleatoire:
+            print("Pensez plus grand.")
+        elif essai > nombre_aleatoire:
+            print("Pensez plus petit.")
+        elif essai == nombre_aleatoire:
+            print("Bravo, vous m'avez eu !")
+            rejouer = input("Voulez vous rejouez ?")
+            if rejouer == "oui":
+                boucle_jeu = False
+            else:
+                print("Merci et aurevoir")
+                boucle_jeu = False
+                start_jeu = False
+
+
+
+
+
+
+
